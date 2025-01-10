b942f113e3cd63c37d940b0622f93afbd1a6ee1b
Eavelie <athallahnaufalr@gmail.com>
2025-01-10 13:16:21 +0700

    refactor logic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-playGame()
+let humanScore = 0
+let computerScore = 0
+let rounds = 0
 
 function getComputerChoice() {
     let randomVal = Math.floor(Math.random() * 3)
@@ -18,64 +20,74 @@ function getHumanChoice() {
     return prompt("Rock/Paper/Scissors?")
 }
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+function playRound(humanChoice, computerChoice) {
 
-    function playRound(humanChoice, computerChoice) {
-        let humanChoiceLC = humanChoice.toLowerCase()
-        let losePrompt = (humanChoice, computerChoice) => `You lose, ${computerChoice} beats ${humanChoice}`
-        let winPrompt = (humanChoice, computerChoice) => `You win, ${humanChoice} beats ${computerChoice}`
-        let drawPrompt = (humanChoice, computerChoice) => `Draw, ${humanChoice} doesn't beat ${computerChoice}`
-    
-        switch(humanChoiceLC) {
-            case "rock":
-                switch(computerChoice) {
-                    case "rock":
-                        console.log(drawPrompt(humanChoiceLC, computerChoice))
-                        break
-                    case "paper":
-                        console.log(losePrompt(humanChoiceLC, computerChoice))
-                        computerScore++
-                        break
-                    case "scissors":
-                        console.log(winPrompt(humanChoiceLC, computerChoice))
-                        humanScore++
-                        break
-                }
-            break
-            case "paper":
-                switch(computerChoice) {
-                    case "rock":
-                        console.log(winPrompt(humanChoiceLC, computerChoice))
-                        humanScore++
-                        break
-                    case "paper":
-                        console.log(drawPrompt(humanChoiceLC, computerChoice))
-                        break
-                    case "scissors":
-                        console.log(losePrompt(humanChoiceLC, computerChoice))
-                        computerScore++
-                        break
-                }
-            break
-            case "scissors":
-                switch(computerChoice) {
-                    case "rock":
-                        console.log(losePrompt(humanChoiceLC, computerChoice))
-                        computerScore++
-                        break
-                    case "paper":
-                        console.log(winPrompt(humanChoiceLC, computerChoice))
-                        humanScore++
-                        break
-                    case "scissors":
-                        console.log(drawPrompt(humanChoiceLC, computerChoice))
-                        break
-                }
-        }
+    let humanChoiceLC = humanChoice.toLowerCase()
+
+    switch(humanChoiceLC) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    showResult('draw', computerChoice, humanChoiceLC)
+                    break
+                case "paper":
+                    showResult('lose', computerChoice, humanChoiceLC)
+                    computerScore++
+                    break
+                case "scissors":
+                    showResult('win', computerChoice, humanChoiceLC)
+                    humanScore++
+                    break
+            }
+        break
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    showResult('win', computerChoice, humanChoiceLC)
+                    humanScore++
+                    break
+                case "paper":
+                    showResult('draw', computerChoice, humanChoiceLC)
+                    break
+                case "scissors":
+                    showResult('lose', computerChoice, humanChoiceLC)
+                    computerScore++
+                    break
+            }
+        break
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    showResult('lose', computerChoice, humanChoiceLC)
+                    computerScore++
+                    break
+                case "paper":
+                    showResult('win', computerChoice, humanChoiceLC)
+                    humanScore++
+                    break
+                case "scissors":
+                    showResult('draw', computerChoice, humanChoiceLC)
+                    break
+            }
     }
 
+    if (rounds === 5) {
+        rounds = 0
+        announceWinner(humanScore, computerScore)
+    } else {
+        rounds++
+    }
+}
+
+function showResult(option, computerChoice, humanChoice) {
+    console.log(
+        option === 'draw' ? `Draw, ${computerChoice} doesn't beat ${humanChoice}` :
+        option === 'win' ? `You win, ${humanChoice} beats ${computerChoice}` :
+        `You lose, ${computerChoice} beats ${humanChoice}`
+    )
+}
+
+function announceWinner(humanScore, computerScore) {
     console.log(
         (humanScore > computerScore) ? "You win the game!" 
         : (computerScore > humanScore) ? "You lose the game!"
